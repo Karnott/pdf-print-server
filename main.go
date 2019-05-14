8066dbf8a7b9c1816a485f9c1aa94911a448beb7
Karnott-G <37289278+geoduf@users.noreply.github.com>
2019-05-14 14:33:57 +0200

    feat(server): create pdf print server

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	pdf "github.com/SebastiaanKlippert/go-wkhtmltopdf"
+)
+
+func main() {
+	http.HandleFunc("/pdf", handlePDF)
+	http.ListenAndServe(":80", nil)
+}
+
+func handlePDF(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		sendError(w, fmt.Sprintf("Expect POST http method, got: %v", r.Method))
+		return
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		sendError(w, err.Error())
+		return
+	}
+	html := bytes.NewBuffer(data)
+	pdf, err := generatePdf(html)
+	if err != nil {
+		sendError(w, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/pdf")
+	_, err = w.Write(pdf.Bytes())
+	if err != nil {
+		sendError(w, err.Error())
+		return
+	}
+}
+
+//
+//
+//
+func sendError(w http.ResponseWriter, errorMessage string) {
+	log.Printf(" Error : %v", errorMessage)
+	w.WriteHeader(400)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": errorMessage,
+	})
+}
+
+//
+//
+//
+func generatePdf(html *bytes.Buffer) (*bytes.Buffer, error) {
+	p, err := pdf.NewPDFGenerator()
+	if err != nil {
+		return nil, err
+	}
+	p.Dpi.Set(600)
+	page := pdf.NewPageReader(html)
+	p.AddPage(page)
+
+	err = p.Create()
+	if err != nil {
+		return nil, err
+	}
+
+	return p.Buffer(), nil
+}
